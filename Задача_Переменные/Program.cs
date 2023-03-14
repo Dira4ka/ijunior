4b7ae08ab8b08df8c06cf135b02d56d8667adb8c
Dira4ka <dira4ka@mail.ru>
2023-03-14 20:36:06 +0500

    Добавьте файлы проекта.

---
+++ Задача_Переменные/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_Переменные
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte AttackRange = 110;
+            short HeroLevel = 100;
+            int HeroExperience = 12350;
+            long Damage = 12000;
+            ulong Armor = 11;
+
+            float Coin = 120.5f;
+            double Health = 1000;
+
+            char Symbol = 'a';
+
+            string PlayerNickname = "chicken1337";
+            bool IsFrendly = true;
+        }
+    }
+}
